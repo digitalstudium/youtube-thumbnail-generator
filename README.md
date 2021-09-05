2c83fa99a11aa0f5310a17dfd7db71eec570da69
Digital Studium <digitalstudium001@gmail.com>
2021-09-05 10:32:58 +0300

    Fixed README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# Youtube Thumbnail Generator
+This python script can create youtube video thumbnails with text on them
+## Installation
+```bash
+pip install -r requirements.txt
+```
+## Usage
+```bash
+python3 main.py <theme> <text on video>
+```
+## Example
+### Command
+```bash
+python3 main.py yoga "Asanas for beginners"
+```
+### Results
+As a result you will get 4 images in a folder containing the script:
+```
+orange_yoga_thumbnail.png
+purple_yoga_thumbnail.png
+white_yoga_thumbnail.png
+yellow_yoga_thumbnail.png
+```
+![Result image 1](example_images/orange_yoga_thumbnail.png?raw=true "Orange")
+![Result image 2](example_images/purple_yoga_thumbnail.png "Purple")
+![Result image 3](example_images/yellow_yoga_thumbnail.png "Yellow")
+![Result image 4](example_images/white_yoga_thumbnail.png "White")
--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python3
 
 import sys
-import time
 import textwrap
-import io
+from io import BytesIO
+from urllib.request import urlopen
 
 from PIL import Image, ImageDraw, ImageFont, ImageColor
-import requests
 
-# background parameters
-width = 1280
-height = 720
-theme = "code"
-background_image_url = f"https://source.unsplash.com/{width}x{height}/?{theme}"
-image_data = io.BytesIO(requests.get(background_image_url, stream=True).content)
-text_background_color = "green"
-margin = 70
-text_background_shape = [(margin, margin), (width - margin, height - margin)]
+# configurable parameters
+background_width = 1280
+background_height = 720
+background_color = "green"
+background_opacity = 128  # number from 0 to 255, where 0 is transparent and 255 is opaque
+background_margin = 70
 
-# text parameters
 font_path = '/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf'
 font_size = 135
 font_colors = ["orange", "yellow", "purple", "white"]
-text = sys.argv[1].upper()
-wrapper = textwrap.TextWrapper(width=10)
+
+text_width = 10  # in symbols
+
+# other parameters
+# background parameters
+theme = sys.argv[1]
+background_image_url = f"https://source.unsplash.com/{background_width}x{background_height}/?{theme}"
+image_data = BytesIO(urlopen(background_image_url).read())
+text_background_shape = [(background_margin, background_margin), (background_width - background_margin, background_height - background_margin)]
+
+# text parameters
+font = ImageFont.truetype(font_path, font_size)
+text = sys.argv[2].upper()
+wrapper = textwrap.TextWrapper(width=text_width)
 word_list = wrapper.wrap(text=text)
 text_on_image = '\n'.join(word_list)
-result_file_name = f"thumbnail-{round(time.time())}.png"
+result_file_name = f"{theme}_thumbnail.png"
 
 # draw images
 for font_color in font_colors:
     image = Image.open(image_data)
-    font = ImageFont.truetype(font_path, font_size)
-    draw = ImageDraw.Draw(image)
-    draw.rectangle(text_background_shape, fill=ImageColor.getrgb(text_background_color))
+    image = image.convert("RGBA")
+    overlay = Image.new('RGBA', image.size, (0, 0, 0) + (0,))
+    draw = ImageDraw.Draw(overlay)  # Create a context for drawing things on it.
+    draw.rectangle(text_background_shape, fill=ImageColor.getrgb(background_color) + (background_opacity,))
     font_width, font_height = draw.textsize(text_on_image, font=font)
-    draw.multiline_text(((width - font_width) / 2, (height - font_height) / 2), text_on_image, font=font,
+    draw.multiline_text(((background_width - font_width) / 2, (background_height - font_height) / 2), text_on_image, font=font,
                         fill=ImageColor.getrgb(font_color), align="center")
 
+    image = Image.alpha_composite(image, overlay)
+    img = image.convert("RGB")
     # save image
-    image.save(f"{font_color}_" + result_file_name, 'PNG')
+    image.save(f"{font_color}_" + result_file_name)
