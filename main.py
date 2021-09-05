aaec8d0144eaf27ce03d10d5f44b07fa22173928
Digital Studium <digitalstudium001@gmail.com>
2021-09-05 17:14:49 +0300

    Rearranged code

--- main.py
+++ main.py
@@ -12,37 +12,38 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# initialize vars
+# handle user input
 theme = sys.argv[1]
+text = sys.argv[2].upper()
+
+# split text into lines
+wrapper = textwrap.TextWrapper(width=config.getint('other', 'text_width'), break_long_words=False)
+text_on_image = wrapper.fill(text=text)
+
+# download image and create corresponding object
 background_image_url = f"https://source.unsplash.com/" \
                        f"{config.getint('background', 'width')}x{config.getint('background', 'height')}/?{theme}"
 image_data = BytesIO(urlopen(background_image_url).read())
-text_background_shape = [(config.getint('background', 'margin'), config.getint('background', 'margin')),
+image = Image.open(image_data).convert("RGBA")
+
+# create background object
+background_shape = [(config.getint('background', 'margin'), config.getint('background', 'margin')),
                          (config.getint('background', 'width') - config.getint('background', 'margin'),
                           config.getint('background', 'height') - config.getint('background', 'margin'))]
+background = Image.new('RGBA', image.size, (0, 0, 0, 0))
 
-# text parameters
+# create font object
 font = ImageFont.truetype(config['font']['path'], config.getint('font', 'size'))
-text = sys.argv[2].upper()
-wrapper = textwrap.TextWrapper(width=config.getint('other', 'text_width'))
-word_list = wrapper.wrap(text=text)
-text_on_image = '\n'.join(word_list)
-result_file_name = f"{theme}_thumbnail.png"
 
-# draw images
+# draw and save images
 for font_color in config['font']['colors'].split(","):
-    image = Image.open(image_data)
-    image = image.convert("RGBA")
-    overlay = Image.new('RGBA', image.size, (0, 0, 0) + (0,))
-    draw = ImageDraw.Draw(overlay)
-    draw.rectangle(text_background_shape,
+    draw = ImageDraw.Draw(background)
+    draw.rectangle(background_shape,
                    fill=ImageColor.getrgb(config['background']['color']) + (config.getint('background', 'opacity'),))
     font_width, font_height = draw.textsize(text_on_image, font=font)
     draw.multiline_text(((config.getint('background', 'width') - font_width) / 2,
                          (config.getint('background', 'height') - font_height) / 2), text_on_image,
                         font=font, fill=ImageColor.getrgb(font_color), align="center")
 
-    image = Image.alpha_composite(image, overlay)
-    img = image.convert("RGB")
-    # save image
-    image.save(f"{config['other']['destination_folder']}{font_color}_" + result_file_name, 'PNG')
+    final_image = Image.alpha_composite(image, background)
+    final_image.save(f"{config['other']['destination_folder']}{font_color}_" + theme + ".png", 'PNG')
